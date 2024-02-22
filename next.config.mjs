3c118754e757152595968a0bea002573634cac42
Junior Real <juniorreal@juniorreal.local>
2024-02-22 22:06:58 +0000

    deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,24 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        hostname: "a0.muscache.com",
+        protocol: "https",
+        port: "",
+      },
+      {
+        hostname: "ovsmkbxstfhfjdghlrrw.supabase.co",
+        protocol: "https",
+        port: "",
+      },
+      {
+        hostname: "avatar.vercel.sh",
+        protocol: "https",
+        port: "",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
